4ac1eb9d54ca835d0a451ed7a9b78e60a80bb910
Mara <Mara@Juans-Mac-mini.local>
2014-06-20 13:01:31 +0200

    Initial Commit

---
+++ EjercicioCoreData/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EjercicioCoreData
+//
+//  Created by Mara on 20/06/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
